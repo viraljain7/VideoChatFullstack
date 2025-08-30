badf00e6a5626cd20859e7ec89ea459b1c337b13
Viral Jain <veerljain1234@gmail.com>
2025-08-30 14:02:43 +0530

    refactor: reorganize imports and streamline middleware setup in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,33 @@
-import express from 'express';
-import 'dotenv/config'
-import cors from 'cors'
+import express from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import path from "path";
-import cookieParser from 'cookie-parser';
-import 'dotenv/config';
-import authRoute from './routes/authRoute.js';
-import userRoute from './routes/userRoute.js';
-import chatRoute from './routes/chatRoute.js';
 
-import { connectDB } from './lib/db.js';
+import authRoutes from "./routes/authRoute.js";
+import userRoutes from "./routes/userRoute.js";
+import chatRoutes from "./routes/chatRoute.js";
 
+import { connectDB } from "./lib/db.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
-
+const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 
-// Middleware
-app.use(cors({ origin: 'http://localhost:5173/',
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
-app.use('/api/auth', authRoute);
-app.use('/api/user', userRoute);
-app.use('/api/chat', chatRoute);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -39,17 +37,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-
-connectDB().then(() => // Start the server
-app.listen(port, async() => {
-    console.log(`Server is running on http://localhost:${port}`);
-}));
-
-// Graceful shutdown
-const shutdown = () => {
-  console.log('Shutting down...');
-  server.close(() => process.exit(0));
-};
-
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
